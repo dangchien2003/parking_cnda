ee18be30782e82833e8c3ab34ef969a5f7cc0844
dangchien2003 <chienboy03@gmail.com>
2024-11-27 14:12:36 +0700

    Update TicketUpdatePlateRequest.java

--- ticket-service/src/main/java/com/parking/ticket_service/dto/request/TicketUpdatePlateRequest.java
+++ ticket-service/src/main/java/com/parking/ticket_service/dto/request/TicketUpdatePlateRequest.java
@@ -3,6 +3,7 @@ package com.parking.ticket_service.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
@@ -12,6 +13,7 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class TicketUpdatePlateRequest {
     @NotBlank(message = "FIELD_INFORMATION_MISSING")
+    @Length(max = 10, message = "Biển số có độ dài tối đã 10 ký tự")
     String plate;
 
     @NotBlank(message = "FIELD_INFORMATION_MISSING")
